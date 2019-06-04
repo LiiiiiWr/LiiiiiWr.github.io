c519dca7e2767d045009dbffe69a8a6e538223c5
LiiiiiWr <331718478@qq.com>
2019-06-04 15:42:28 +0800

    Site updated: 2019-06-04 15:42:24

--- js/src/custom.js
+++ js/src/custom.js
@@ -5,6 +5,10 @@
           "/images/background/2.jpg",
           "/images/background/3.png",
           "/images/background/4.png",
-          "/images/background/5.png"
+          "/images/background/5.png",
+          "/images/background/6.jpg",
+          "/images/background/7.jpg",
+          "/images/background/8.jpg",
         ], { duration: 30000, fade: 1500 });  
     });
+  
