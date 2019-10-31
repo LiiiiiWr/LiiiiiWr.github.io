ac89807de66a6a8a4eb8e6638316a5f14ea1665b
LiiiiiWr <331718478@qq.com>
2019-10-31 10:38:01 +0800

    Site updated: 2019-10-31 10:38:00

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,14 +1,14 @@
 /* 轮播背景图片 */
     $(function () {
         $.backstretch([  
-          "/images/background/1.jpg",
-          "/images/background/2.jpg",
-          "/images/background/3.png",
-          "/images/background/4.png",
-          "/images/background/5.png",
-          "/images/background/6.jpg",
-          "/images/background/7.jpg",
-          "/images/background/8.jpg",
+          "/images/background/1.jpg?width={width}&height={height}",
+          "/images/background/2.jpg?width={width}&height={height}",
+          "/images/background/3.png?width={width}&height={height}",
+          "/images/background/4.png?width={width}&height={height}",
+          "/images/background/5.png?width={width}&height={height}",
+          "/images/background/6.jpg?width={width}&height={height}",
+          "/images/background/7.jpg?width={width}&height={height}",
+          "/images/background/8.jpg?width={width}&height={height}",
         ], { duration: 30000, fade: 1500 });  
     });
   
